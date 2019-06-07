6a11c00a2d04733154c4d6b20a32dd21b2617960
amisha <amisha@callrail.com>
2019-06-07 13:31:03 -0400

    run sonarpython test

--- port_forward.sh
+++ port_forward.sh
@@ -6,6 +6,6 @@ chmod 400 ../autonomiq-test.pem
 ssh -o StrictHostKeyChecking=no -i ../autonomiq-test.pem ubuntu@ec2-54-183-112-83.us-west-1.compute.amazonaws.com "sudo fuser -k $remote_port/tcp"
 ssh -fN -o StrictHostKeyChecking=no -vv -R $remote_port:localhost:$app_port -i ../autonomiq-test.pem ubuntu@ec2-54-183-112-83.us-west-1.compute.amazonaws.com
 ##-f    Requests ssh to go to background just before command execution.
-##-N    Do not execute a remote command.
+##-N    Don't execute a remote command.
 
 
